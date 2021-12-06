3be0a3745d6a53fc9575b4c67932d3817c2ac27a
Tr00d <guillaume.faas@live.fr>
2021-12-06 09:47:45 +0100

    Using composition to remove duplication -- filling readme file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CleanEventSourcing
+
+## Summary
+This project came up as a personal challenge. This is a simple ToDoList api with CRUD and Event Sourcing like we can find many on GitHub.
+
+
+Still, every single implementation I saw were using either Reflection or Switches (sometimes Dictionary but the problem remains the same) to construct an aggregate based on previous events.
+I wanted to try a clean**er** approach.
+
+My proposal comes with a compromise on the workflow: an event applies its logic on an aggregate. 
+This is not really a bad compromise as it would allow us to creates more events without affecting the aggregate (OCP).
+Keeping that in mind, I kept the logic in the Aggregate.Apply method for encapsulation reasons: an event calls the aggregate when passing himself as a parameter.
+
+I hope you'll find this implementation interesting. 
+
+
--- src/CleanEventSourcing.Domain/Items/Events/UpdatedItemEvent.cs
+++ src/CleanEventSourcing.Domain/Items/Events/UpdatedItemEvent.cs
@@ -6,6 +6,8 @@ namespace CleanEventSourcing.Domain.Items.Events
 {
     public class UpdatedItemEvent : IIntegrationEvent<Item>
     {
+        private readonly ItemEventBase eventBase = new();
+        
         public UpdatedItemEvent(Guid id, Option<string> oldDescription, Option<string> newDescription)
         {
             this.Id = id;
@@ -22,9 +24,9 @@ namespace CleanEventSourcing.Domain.Items.Events
         public void Apply(Option<Item> aggregate) => aggregate.IfSome(value => value.Apply(this));
         public Option<string> Stream { get; set; }
 
-        public bool CanConvertTo<T>() where T : IAggregate => typeof(T) == typeof(Item);
+        public bool CanConvertTo<T>() where T : IAggregate => this.eventBase.CanConvertTo<T>();
 
         public Option<IIntegrationEvent<T>> ConvertTo<T>() where T : IAggregate =>
-            this.CanConvertTo<T>() ? Some((IIntegrationEvent<T>) this) : Option<IIntegrationEvent<T>>.None;
+            this.eventBase.ConvertTo((IIntegrationEvent<T>) this);
     }
 }
